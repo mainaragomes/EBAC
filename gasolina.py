8c400b826a104832465f0a434f880cc3cd31b148
mainaragomes <mainaragommes@hotmail.com>
2024-07-24 17:24:48 +0000

    Exercicio EBAC Módulo 18

--- gasolina.py
+++ gasolina.py
@@ -1,12 +1,13 @@
 
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Lê os dados do arquivo CSV para um DataFrame
-conteudo = pd.read_csv('gasolina.csv')
+df = pd.read_csv('gasolina.csv')
 
 # Cria o gráfico de linha com Seaborn
-with sns.axes_style('whitegrid'):
-    grafico = sns.lineplot(data=conteudo, x='dia', y='venda', palette='dark')
-    grafico.set(title='Preço da gasolina', xlabel='Dia', ylabel='Preço')
-    grafico.figure.savefig('gasolina.png')
+sns.lineplot(x='dia', y='venda', data=df)
+
+# Salva o gráfico como uma imagem PNG
+plt.savefig('gasolina.png')
